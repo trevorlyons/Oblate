401b0c825bf30ee8e6e485da852602e862dca87a
trevorlyons <trevorjclyons@hotmail.com>
2018-02-24 12:08:10 +0100

    Initial Commit

---
+++ Oblate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Oblate
+//
+//  Created by Trevor Lyons on 2018-02-24.
+//  Copyright © 2018 Trevor Lyons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

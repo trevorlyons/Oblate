b8f48d55bda8d69409011678128129fdbdbb0a59
trevorlyons <trevorjclyons@hotmail.com>
2018-03-08 17:15:12 +0100

    PickerVC created with delegate to send data

---
+++ Oblate/Resources/languageArray.swift
@@ -0,0 +1,9 @@
+//
+//  languageArray.swift
+//  Oblate
+//
+//  Created by Trevor Lyons on 2018-03-08.
+//  Copyright © 2018 Trevor Lyons. All rights reserved.
+//
+
+import Foundation
---
+++ Oblate/View/RoundedSideButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedSideButton.swift
+//  Oblate
+//
+//  Created by Trevor Lyons on 2018-03-08.
+//  Copyright © 2018 Trevor Lyons. All rights reserved.
+//
+
+import UIKit
+
+class RoundedSideButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
